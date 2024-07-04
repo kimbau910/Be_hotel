94e355389fd556dad134f8aadac371b2ea0686fb
Ciau <2051063478@e.tlu.edu.vn>
2024-07-04 18:57:04 +0700

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const useRoute = require("./useRoute");
-const DetailRouter = require("./DetailRouter");
+const detailRoute = require("./detailRoute");
 const routes = (app) => {
   app.use("/api/user", useRoute);
-  app.use("/api/detail", DetailRouter);
+  app.use("/api/detail", detailRoute);
 };
 module.exports = routes;  
