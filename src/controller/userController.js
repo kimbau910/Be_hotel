332a0e0e7675fff3e1abc0a7a8d545a1cb4fe949
Ciau <2051063478@e.tlu.edu.vn>
2024-07-04 18:11:34 +0700

    fix

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,4 +1,4 @@
-const UserService = require("../services/UserService");
+const UserService = require("../services/userService");
 const JwtService = require("../services/JwtService");
 
 const createUser = async (req, res) => {
@@ -28,35 +28,35 @@ const createUser = async (req, res) => {
 
 const loginUser = async (req, res) => {
   try {
-      const { email, password } = req.body
-      const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
-      const isCheckEmail = reg.test(email)
-      if (!email || !password) {
-          return res.status(200).json({
-              status: 'ERR',
-              message: 'The input is required'
-          })
-      } else if (!isCheckEmail) {
-          return res.status(200).json({
-              status: 'ERR',
-              message: 'The input is email'
-          })
-      }
-      const response = await UserService.loginUser(req.body)
-      const { refresh_token, ...newReponse } = response
-      res.cookie('refresh_token', refresh_token, {
-          httpOnly: true,
-          secure: false,
-          sameSite: 'strict',
-          path: '/',
-      })
-      return res.status(200).json({...newReponse, refresh_token})
+    const { email, password } = req.body;
+    const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    const isCheckEmail = reg.test(email);
+    if (!email || !password) {
+      return res.status(200).json({
+        status: "ERR",
+        message: "The input is required",
+      });
+    } else if (!isCheckEmail) {
+      return res.status(200).json({
+        status: "ERR",
+        message: "The input is email",
+      });
+    }
+    const response = await UserService.loginUser(req.body);
+    const { refresh_token, ...newReponse } = response;
+    res.cookie("refresh_token", refresh_token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+      path: "/",
+    });
+    return res.status(200).json({ ...newReponse, refresh_token });
   } catch (e) {
-      return res.status(404).json({
-          message: e
-      })
+    return res.status(404).json({
+      message: e,
+    });
   }
-}
+};
 const updateUser = async (req, res) => {
   try {
     const userId = req.params.id;
@@ -75,7 +75,6 @@ const updateUser = async (req, res) => {
     });
   }
 };
-
 
 const deleteUser = async (req, res) => {
   try {
@@ -143,7 +142,6 @@ const getDetailsUser = async (req, res) => {
 };
 
 const refreshToken = async (req, res) => {
-  
   try {
     let token = req.headers.token.split(" ")[1];
     if (!token) {
--- src/services/Jwtservice.js
+++ src/services/Jwtservice.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
 const dotenv = require('dotenv');
+
 dotenv.config()
 
 const genneralAccessToken = async (payload) => {
